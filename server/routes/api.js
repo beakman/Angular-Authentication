a29b93e5dab975f6fbc76caed0c25f596a293019
Imran Khan <imransilvake@gmail.com>
2018-06-04 21:26:31 +0200

    Remove user

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const register = require('./register');
 const login = require('./login');
 const events = require('./events');
-const user = require('./user');
 
 // routes
 router.get('/', (req, res) => res.send('Application Route!'));
 router.use(register);
 router.use(login);
 router.use(events);
-router.use(user);
 
 module.exports = router;
